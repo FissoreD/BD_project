826b36de2ca29ac9729e67255b1e64f17ca50f3e
FissoreD <79260701+FissoreD@users.noreply.github.com>
2021-12-27 12:30:14 +0100

    Creation du fichier pour les requetes

---
+++ ProjetBDrequetes.sql
@@ -0,0 +1,20 @@
+-- Requetes de consultations
+-- 5 requetes impliquant 1 table dont 1 avec un group By et une avec un Order By
+-- 5 requetes impliquant 2 tables avec jointures internes dont 1 externe + 1 group by + 1 tri
+-- 5 requetes impliquant plus de 2 tables avec jointures internes dont 1 externe + 1 group by + 1 tri
+
+-- Here code
+
+-- Requetes de mise a jour
+-- 2 requetes impliquant 1 table
+-- 2 requetes impliquant 2 tables
+-- 2 requetes impliquant plus de 2 tables
+
+-- Here code
+
+-- Requetes de suppression 
+-- 2 requetes impliquant 1 table
+-- 2 requetes impliquant 2 tables
+-- 2 requetes impliquant plus de 2 tables
+
+-- Here code
