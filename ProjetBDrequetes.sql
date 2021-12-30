6db256ebaa75fba4cce280f495d54134941ecaeb
Antoine Venturelli <ant.venturelli06@gmail.com>
2021-12-30 10:39:55 +0100

    Requêtes de consultations sur 1 table

--- ProjetBDrequetes.sql
+++ ProjetBDrequetes.sql
@@ -1,9 +1,63 @@
 -- Requetes de consultations
 -- 5 requetes impliquant 1 table dont 1 avec un group By et une avec un Order By
--- 5 requetes impliquant 2 tables avec jointures internes dont 1 externe + 1 group by + 1 tri
--- 5 requetes impliquant plus de 2 tables avec jointures internes dont 1 externe + 1 group by + 1 tri
 
--- Here code
+-- Les cartes dont la remise est >= à 0.4
+SELECT
+    oc.nom
+FROM
+    carte_o oc
+WHERE
+    oc.remise >= 0.4;
+
+-- Nom et prenom de chaque fournisseur
+SELECT
+    nom,
+    prenom
+FROM
+    fournisseur_o;
+
+-- Fournisseur qui n'ont plus de factures à payer
+SELECT
+    *
+FROM
+    fournisseur_o of
+WHERE
+    (
+        SELECT
+            COUNT(*)
+        FROM
+            of.get_factures_a_payer()
+    ) != 0;
+
+-- Requête avec regroupement sur les jobs
+SELECT
+    job,
+    SUM(salaire)
+FROM
+    empl_o oe
+GROUP BY
+    job;
+
+-- Requête avec tri utilisant notre méthode map
+SELECT
+    oa.pays,
+    od.ville
+FROM
+    adresse_o oa
+WHERE
+    oa.numero = 17000
+    OR oa.numero = 19000
+ORDER BY
+    value(oa);
+
+
+-- 5 requetes impliquant 2 tables avec jointures internes dont 1 externe + 1 group by + 1 tri
+--SELECT *
+--FROM
+--         empl_o oe
+--    INNER JOIN adresse_o oa ON oe.adresse = oa;
+
+-- 5 requetes impliquant plus de 2 tables avec jointures internes dont 1 externe + 1 group by + 1 tri
 
 -- Requetes de mise a jour
 -- 2 requetes impliquant 1 table
